5a8f0d8470060ed93f4b48e1e8a7a2ceea54e0c0
Jiaying0220 <jiaying.kou.2019@sis.smu.edu.sg>
2021-10-23 21:50:14 +0800

    Add course leaner js

---
+++ app/js/course_page_learner.js
@@ -0,0 +1,46 @@
+const couse_app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+      course: [{
+          'course_no': 1,
+          'session': [{
+              'session_no': [1, 2, 3],
+              'lesson': [{
+                  'lesson_no': [1, 2, 3],
+                  'quiz': 'Quiz 1',
+                  'material':[{
+                      'material_list': [1, 2, 3]
+                  }]
+                  }]
+              }]
+          },
+          {
+            'course_no': 2,
+            'session': [{
+                'session_no': [1, 2, 3],
+                'lesson': [{
+                    'lesson_no': [1, 2, 3],
+                    'quiz': 'Quiz 2',
+                    'material':[{
+                        'material_list': [1, 2, 3]
+                    }]
+                    }]
+                }]
+            },
+            {
+                'course_no': 3,
+                'session': [{
+                    'session_no': [1, 2, 3],
+                    'lesson': [{
+                        'lesson_no': [1, 2, 3],
+                        'quiz': 'Quiz 3',
+                        'material':[{
+                            'material_list': [1, 2, 3]
+                        }]
+                        }]
+                    }]
+                }]
+    }
+})
+        

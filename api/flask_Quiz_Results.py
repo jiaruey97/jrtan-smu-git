698804ea6a29595410a0000a8d3762cd1c29b93c
hxchen-2019 <92243924+hxchen-smu@users.noreply.github.com>
2021-10-25 18:37:54 +0800

    Update to Bitnami
    - Minor Code Correction (Change configuration for Bitnami)

--- api/flask_Quiz_Results.py
+++ api/flask_Quiz_Results.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 CORS(app)
 
 #local flask
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/spm'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/spm'
 
 #bitnami flask
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:t2AlF2wAibZH@127.0.0.1:3306/SPM'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:t2AlF2wAibZH@127.0.0.1:3306/SPM'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 

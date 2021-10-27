f816238fbea9b0d05eb6bc598d2b222df99b3f06
Jiaying0220 <jiaying.kou.2019@sis.smu.edu.sg>
2021-10-27 13:16:09 +0800

    change material url

--- app/js/course_page_learner.js
+++ app/js/course_page_learner.js
@@ -42,14 +42,14 @@ const course_page = new Vue({
         greet: function (event){
           //alert("hi")
           axios({
-            url: 'http://localhost/spm_project/material1.txt',
+            url: 'http://localhost/jrtan-smu-git/testing.txt',
             method: 'GET',
             responseType: 'blob'
           }).then((response) => {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', 'material1.txt');
+            link.setAttribute('download', 'material');
             document.body.appendChild(link);
             link.click();
             });

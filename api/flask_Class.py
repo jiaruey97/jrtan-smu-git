78d6ad48be2d5dc3b5d91d358b7d3fd46941ee27
jiaruey97 <jiaruey.tan.2019@sis.smu.edu.sg>
2021-11-01 21:49:14 +0800

    test check all

--- api/flask_Class.py
+++ api/flask_Class.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = Flask(__name__), ,
+app = Flask(__name__)
 CORS(app)
 
 #local flask

2347b5f4ee02bb4ae8decaf3568e693787dabdb2
wyt05 <90769809+wyt05@users.noreply.github.com>
2021-11-04 11:59:38 +0800

    tracker

--- api/flask_Tracker.py
+++ api/flask_Tracker.py
@@ -6,10 +6,10 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-#local flask
+# local flask
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/spm'
 
-#bitnami flask
+# bitnami flask
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:t2AlF2wAibZH@127.0.0.1:3306/SPM'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,14 +21,10 @@ class Tracker(db.Model):
     __tablename__ = 'Tracker'
 
     Tracker_ID = db.Column(db.Integer, primary_key=True)
-    # Username = db.Column(db.String(50), db.ForeignKey('User_Database.Username'))
-    # Course_ID = db.Column(db.Integer, db.ForeignKey('Course.Course_ID'))
-    # Class_ID = db.Column(db.Integer, db.ForeignKey('Class.Class_ID'))
     Username = db.Column(db.String(50))
     Course_ID = db.Column(db.Integer)
     Class_ID = db.Column(db.Integer)
-    Section_Object = db.Column(db.Text, nullable=False)
-
+    Section_Object = db.Column(db.Text)
 
     def __init__(self, Username, Course_ID, Class_ID, Section_Object):
         #self.Tracker_ID = Tracker_ID
@@ -39,6 +35,7 @@ class Tracker(db.Model):
 
     def json(self):
         return {"Tracker_ID": self.Tracker_ID, "Username": self.Username, "Course_ID": self.Course_ID, "Class_ID": self.Class_ID, "Section_Object": self.Section_Object}
+
 
 @app.route("/spm/tracker")
 def get_all_tracker():
@@ -59,15 +56,41 @@ def get_all_tracker():
         }
     ), 404
 
-@app.route("/spm/get_tracker/<string:username>")
-def retrieve_user_track():
-    data = request.get_json()
 
-#Initialize the tracker for user
+@app.route("/spm/get_tracker/<string:username>/<int:course_id>/<int:class_id>")
+def retrieve_user_tracking_details(username, course_id, class_id):
+    tracker = Tracker.query.filter_by(
+        Username=username,
+        Course_ID=course_id,
+        Class_ID=class_id
+    ).first()
+    print(tracker.json())
+    print(tracker.json()["Section_Object"])
+    if tracker:
+        return jsonify(
+            {
+                "code": 200,
+                "data": tracker.json()
+            }
+        ), 200
+    return jsonify(
+        {
+            {
+                "code": 404,
+                "message": "Specified tracker cannot be found."
+            }
+        }
+    ), 404
+
+{"sections_cleared": 0, "quiz_cleared": 0}
+# Initialize the tracker for user
+
+
 @app.route("/create_tracker/<string:username>/<int:course_id>/<int:class_id>")
 def create_tracker(username, course_id, class_id):
     json_track = json.dumps({'sections_cleared': 0, 'quiz_cleared': 0})
-    tracker = Tracker(Username=username, Course_ID=course_id, Class_ID=class_id, Section_Object=json_track)
+    tracker = Tracker(Username=username, Course_ID=course_id,
+                      Class_ID=class_id, Section_Object=json_track)
     db.session.add(tracker)
 
     try:
@@ -79,7 +102,7 @@ def create_tracker(username, course_id, class_id):
                 "message": "An error occurred creating unique tracking id for user."
             }
         ), 500
-    
+
     return jsonify(
         {
             'code': 200,
@@ -87,9 +110,6 @@ def create_tracker(username, course_id, class_id):
         }
     ), 200
 
-
-
-    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5644, debug=True)

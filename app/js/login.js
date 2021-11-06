454274ada81fa5267f8d4d4f8e77c1199d28a86d
Jiaying0220 <email@example.com>
2021-11-07 00:52:06 +0800

    login update

--- app/js/login.js
+++ app/js/login.js
@@ -1,10 +1,45 @@
+const courseAddress = '3.131.65.207:5144'
+const materialAddress = '3.131.65.207:5344'
+const classAddress='3.131.65.207:5044'
+const userAddress='3.131.65.207:5744'
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+const params = Object.fromEntries(urlSearchParams.entries())
+
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     computed: {
-      passwordMatch() {
-        return () => this.password === this.verify || "Password must match";
+      usernameMatch() {
+        return () => this.Username === this.verify || "username must match";
       }
+    },
+    data: {
+      Username: '',
+      course_id:params.course_id,
+      // username = '',
+      // Actual_Name: params.Actual_Name,
+      // Course_Assigned: params.Course_Assigned,
+      // Course_Completed: params.Course_Completed,
+      // Course_Pending: params.Course_Completed,
+      // Current_Position: params.Current_Position,
+      // Department: params.Department,
+      // chosen_course_name: params.course_name,
+    },
+    created() {
+      // axios.get(`http://${materialAddress}/spm/materials/${this.course_id}`)
+      axios.get(`http://${userAddress}/spm/user_database/${this.Username}`)
+          .then(function (response) {
+            return_response = response.data.data
+            // console.log(course_id)
+            console.log(Username)
+  
+            vueApp.validate()
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+  
     },
     methods: {
       validate() {
@@ -17,6 +52,18 @@ new Vue({
       },
       resetValidation() {
         this.$refs.form.resetValidation();
+      },
+      redirect() {
+        // this.$router.push({name: ''})
+        if("[user_database.Current_Position]" == "Instructor"){
+          window.location = "instructor_assign.html";
+          }
+        else if ("[user_database.Current_Position]" == "HR"){
+          window.location = "HR_webpage.html";
+          }
+        else{
+          window.location = "course_enrolment_page.html?user=Tommy";
+          }
       }
     },
     data: () => ({
@@ -37,18 +84,21 @@ new Vue({
       loginEmail: "",
       loginEmailRules: [
         v => !!v || "Required",
-        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+        // v => /.+@.+\..+/.test(v) || "E-mail must be valid"
       ],
       emailRules: [
         v => !!v || "Required",
-        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+        // v => /.+@.+\..+/.test(v) || "E-mail must be valid"
       ],
   
       show1: false,
       rules: {
         required: value => !!value || "Required.",
-        min: v => (v && v.length >= 8) || "Min 8 characters"
+        // min: v => (v && v.length >= 8) || "Min 8 characters"
       }
     })
   });
+
+
+  
   
